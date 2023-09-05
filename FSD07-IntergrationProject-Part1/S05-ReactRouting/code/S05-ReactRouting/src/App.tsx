9c8cbdc8cbb16f3ba69771dc88d250a9f294a333
jonney2021 <yeminghuhym@gmail.com>
2023-09-04 23:05:15 -0400

    S05-ReactRouting

---
+++ FSD07-IntergrationProject-Part1/S05-ReactRouting/code/S05-ReactRouting/src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+export const App = () => {
+  // create a bootstrap navbar
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </div>
+  );
+};
