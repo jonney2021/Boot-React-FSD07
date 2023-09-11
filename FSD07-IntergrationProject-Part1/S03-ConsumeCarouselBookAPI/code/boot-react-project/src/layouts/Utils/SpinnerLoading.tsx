33adc81246ce8e083ce9bf9ef7892110cffb0bb2
ChihYin Chen <118857398+jean9947@users.noreply.github.com>
2023-09-11 10:55:40 -0400

    added TODO

--- FSD07-IntergrationProject-Part1/S03-ConsumeCarouselBookAPI/code/boot-react-project/src/layouts/Utils/SpinnerLoading.tsx
+++ FSD07-IntergrationProject-Part1/S03-ConsumeCarouselBookAPI/code/boot-react-project/src/layouts/Utils/SpinnerLoading.tsx
@@ -1,10 +1,12 @@
 export const SpinnerLoading = () => {
+  // TODO: Add props for configurable size and color
   return (
     <div
       className="container m-5 d-flex justify-content-center"
       style={{ height: 550 }}
     >
       <div className="spinner-border text-primary" role="status">
+        // TODO: Improve accessibility by providing more informative hidden text
         <span className="visually-hidden">Loading...</span>
       </div>
     </div>
