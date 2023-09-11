4d382703faef3a0ad429f0ad77010025ec00c5c3
ChihYin Chen <118857398+jean9947@users.noreply.github.com>
2023-09-11 16:08:46 -0400

    added TODO

--- FSD07-IntergrationProject-Part1/S08-Authentication/code/S08-AuthenticationReact/src/Auth/LoginWidget.jsx
+++ FSD07-IntergrationProject-Part1/S08-Authentication/code/S08-AuthenticationReact/src/Auth/LoginWidget.jsx
@@ -4,17 +4,20 @@ import { SpinnerLoading } from "../layouts/Utils/SpinnerLoading";
 import OktaSignInWidget from "./OktaSignInWidget";
 
 const LoginWidget = ({ config }) => {
-  const { oktaAuth, authState } = useOktaAuth();
+  const { oktaAuth, authState } = useOktaAuth(); // TODO: Integrate with Redux or another state management library.
   const onSuccess = (tokens) => {
-    oktaAuth.handleLoginRedirect(tokens);
+    oktaAuth.handleLoginRedirect(tokens); // TODO: Add more conditional logic for different redirects based on user roles or other factors.
   };
 
   const onError = (err) => {
+    // TODO: Add detailed logs and integrate with a monitoring system.
     console.log("Sign in error: ", err);
-  };
+  }; 
+  // TODO: Implement user-friendly error messages for different kinds of login errors.
 
   if (!authState) {
     return <SpinnerLoading />;
+    // TODO: Implement more engaging loading animations or states.
   }
 
   return authState.isAuthenticated ? (
