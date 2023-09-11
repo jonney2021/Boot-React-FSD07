3836fa14c6f7c1b16ab73c48a84388a33bc4d438
ChihYin Chen <118857398+jean9947@users.noreply.github.com>
2023-09-11 16:20:39 -0400

    added TODO

--- FSD07-IntergrationProject-Part1/S09-HomepageRefactorWithAuth/code/S09-HomepageRefactorWithAuth/src/layouts/HomePage/components/ReturnBook.tsx
+++ FSD07-IntergrationProject-Part1/S09-HomepageRefactorWithAuth/code/S09-HomepageRefactorWithAuth/src/layouts/HomePage/components/ReturnBook.tsx
@@ -7,6 +7,7 @@ export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
     <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
       <div className="text-center">
         {props.book.img ? (
+      // TODO: Implement lazy loading for images.
           <img src={props.book.img} alt="book" width="151" height="233" />
         ) : (
           <img
@@ -18,11 +19,13 @@ export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
         )}
 
         <h6 className="mt-2">{props.book.title}</h6>
+        {/* TODO: Handle cases where the book title or author is not available. */}
         <p>{props.book.author}</p>
         <Link
           className="btn main-color text-white"
           to={`checkout/${props.book.id}`}
         >
+          {/* TODO: Disable the 'Reserve' button if the book is not available for checkout. */}
           Reserve
         </Link>
       </div>
