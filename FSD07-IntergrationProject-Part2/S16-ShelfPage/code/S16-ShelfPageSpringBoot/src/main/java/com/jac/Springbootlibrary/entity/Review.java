47038e6102a1a09e6eb8b517705bdd51e1867e93
SiyiC41 <csyclara0122@gmail.com>
2023-09-10 21:58:53 -0400

    Add S16-ShelfPage

---
+++ FSD07-IntergrationProject-Part2/S16-ShelfPage/code/S16-ShelfPageSpringBoot/src/main/java/com/jac/Springbootlibrary/entity/Review.java
@@ -0,0 +1,37 @@
+package com.jac.Springbootlibrary.entity;
+
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "review")
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "date")
+    @CreationTimestamp
+    private Date date;
+
+    @Column(name = "rating")
+    private double rating;
+
+    @Column(name = "book_id")
+    private Long bookId;
+
+    @Column(name = "review_description")
+    private String reviewDescription;
+
+}
